b0e1ceeb6299b33bcbdeb45a8366f279be78b18f
kidboi666 <kidboi666@gmail.com>
2025-03-28 14:23:45 +0900

    remove : 미사용 컴포넌트 AppInitializer.tsx 삭제

--- src/core/components/AppInitializer.tsx
+++
@@ -1,5 +0,0 @@
-import { useApp } from '@/core/store/contexts/app.context';
-
-export const AppInitializer = () => {
-  const { initAppData, isInitialApp, firstLaunchDate, isLoading } = useApp();
-};
